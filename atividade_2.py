b4c2c386bc71f34d0c1ff72d6df80ef20b36d764
Leonardo Guerreiro da Silva <dleonardoguerreiro@gmail.com>
2026-09-03 23:57:16 +0000

    Solução

---
+++ atividade_2.py
@@ -0,0 +1,47 @@
+lista = [
+    (3, 'Ana'), (10, 'Bruno'), (15, 'Carlos'), (18, 'Daniela'), (19, 'Eduardo'),
+    (28, 'Fernanda'), (33, 'Gustavo'), (35, 'Helena'), (43, 'Igor'), (48, 'Juliana'),
+    (58, 'Kleber'), (83, 'Larissa'), (84, 'Marcos'), (86, 'Natália'), (97, 'Otávio'),
+    (104, 'Patrícia'), (106, 'Rafael'), (115, 'Sabrina'), (120, 'Tiago'), (122, 'Vanessa'),
+    (127, 'Amanda'), (143, 'Breno'), (147, 'Camila'), (149, 'Diego'), (151, 'Eliane'),
+    (175, 'Fabiano'), (179, 'Gabriela'), (184, 'Henrique'), (187, 'Isabela'), (194, 'João'),
+    (199, 'Karen'), (201, 'Leonardo'), (211, 'Mirela'), (213, 'Nicolas'), (232, 'Olívia'),
+    (241, 'Pedro'), (244, 'Queila'), (246, 'Rodrigo'), (256, 'Simone'), (258, 'Túlio'),
+    (259, 'Ursula'), (261, 'Victor'), (269, 'Wesley'), (273, 'Xênia'), (278, 'Yasmin'),
+    (280, 'Zeca'), (288, 'Alana'), (291, 'Caio'), (292, 'Diana'), (294, 'Fábio')
+]
+
+def pesquisa_binaria(lista, item):
+  baixo = 0
+  alto = len(lista) - 1
+  tentativas = 0
+
+  while baixo <= alto:
+    tentativas += 1
+    meio = (baixo + alto) // 2
+    chute = lista[meio][0]
+    if chute == item:
+      print(f"As tentativas necessárias pela PESQUISA BINÁRIA foi: {tentativas}")
+      return meio
+    elif chute > item:
+      alto = meio - 1
+    else:
+      baixo = meio + 1
+  return None
+
+def pesquisa_sequencial(lista, alvo):
+    tentativas2 = 0
+    for posicao, item in enumerate(lista):
+        tentativas2 += 1
+        if item[0] == alvo:
+            print(f"A quantidade de tentativas pela PESQUISA SEQUENCIAL foi: {tentativas2}")
+            return posicao
+    else:
+        return -1    
+
+resultado_binaria = pesquisa_binaria(lista, 256)
+print(f"O nome associado ao ID (Binária) é {lista[resultado_binaria][1]}\n")
+
+# Testando a Pesquisa Sequencial
+resultado_sequencial = pesquisa_sequencial(lista, 256)
+print(f"O nome associado ao ID (Sequencial) é {lista[resultado_sequencial][1]}")
